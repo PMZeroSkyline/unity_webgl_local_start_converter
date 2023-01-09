c9974f411b9802d2c310eba41af515fb70fe1e6e
zhangzb <guzhang0@gmail.com>
2023-01-09 16:47:35 +0800

    set ico

--- forge.config.js
+++ forge.config.js
@@ -1,22 +1,38 @@
+const path = require('path');
+
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    icon: path.join(__dirname, 'icon')
+  },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        iconUrl: path.join(__dirname, 'icon.ico'),
+      },
     },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
+      config:{
+        options: {
+        }
+      },
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config:{
+        options: {
+        }
+      },
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      config:{
+        options: {
+        }
+      },
     },
   ],
 };
